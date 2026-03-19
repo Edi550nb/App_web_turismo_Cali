d551bb1a080e707385271044443fc0a169e7e557
Edi550nb <88601614+Edi550nb@users.noreply.github.com>
2026-03-18 22:41:27 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,45 @@
+function showSection(id){
+ 
+document.querySelectorAll(".section").forEach(section=>{
+section.style.display="none";
+});
+ 
+document.getElementById(id).style.display="block";
+}
+ 
+let language="en";
+ 
+function toggleLanguage(){
+ 
+language = language==="en" ? "es" : "en";
+document.querySelectorAll("[data-en]").forEach(element=>{
+element.textContent = element.getAttribute("data-"+language);
+});
+}
+
+var map = L.map("map").setView([3.4516,-76.5320],13);
+L.tileLayer(
+ 
+"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+{
+attribution:"© OpenStreetMap"
+}
+).addTo(map);
+ 
+ 
+ 
+var locations=[
+ 
+[3.4516,-76.5320,"Plaza de Cayzedo"],
+[3.4528,-76.5319,"La Ermita"],
+[3.4535,-76.5326,"Boulevard del Río"],
+[3.433,-76.545,"Cristo Rey"]
+];
+
+locations.forEach(function(place){
+L.marker([place[0],place[1]])
+.addTo(map)
+.bindPopup(place[2]);
+});
+
+showSection("culture");
